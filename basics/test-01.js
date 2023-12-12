29700167cea51a5933277a5dd5a511b32783581f
ritik-apurwa <ritikapurwa3500@gmail.com>
2023-12-12 15:45:09 +0530

    ss

---
+++ basics/test-01.js
@@ -0,0 +1,128 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+// console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+// console.log(3 
+//     +
+//      3) // code readability should be high
+
+// console.log("Hitesh") 
+
+
+// let name = "hitesh"
+// let age = 18
+// let isLoggedIn = false
+// let state;
+
+// number => 2 to power 53 this length can be saved in i const 
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique to show uniqness of an element 
+
+
+// object
+
+// console.log(typeof undefined); // undefined
+// console.log(typeof null); // object
+
+
+
+let score = "hitesh"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let change_score_value_in_number = Number(score)
+// console.log(typeof change_score_value_in_number);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// // 1 => true; 0 => false
+// // "" => false
+// // "hitesh" => true
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// // *********************** Operations ***********************
+
+// let value = 3
+// let negValue = -value
+// // console.log(negValue);
+
+
+// ************************* Simple  Mathmatical Calculations ********************** 
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**9);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1 = "hello"
+// let str2 = " hitesh"
+
+// let str3 = str1 + str2
+// // console.log(str3);
+ // **********************************   logical Mathamatical Operations *********************
+// console.log("1" + 2 + 2);
+// console.log(1 + "2");
+// console.log("1" + "2"  + 2 - "124");
+// console.log(1 + 2 + "2");
+
+// console.log( (0 + 2) **3 ); 
+
+// * means multiply ** and then number means in this example four's power seven if there is written **7 
+
+// // console.log(+true);
+// // console.log(+"");
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+
+// let gameCounter = 100
+// ++gameCounter;
+// console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
