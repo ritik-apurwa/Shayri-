27ecbddbc241e13b54e78943c3dbaf7f9b2f9c63
ritik-apurwa <ritikapurwa3500@gmail.com>
2023-12-14 12:38:50 +0530

    new project assembled

---
+++ my-project/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+        <div className='bg-red-300 text-yellow-200' >
+            <h1 id='pahla-h1'> First heading </h1>
+            <a href="/Home.jsx">Home</a>
+        </div>
+    </>
+  )
+}
+
+export default App
---
+++ my-project/src/main.jsx
@@ -0,0 +1,14 @@
+import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div >
+      i am Navbar 
+    <App />
+    </div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
