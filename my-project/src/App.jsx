b0e18073a2f7f78880963214a17a49fd90ae5d98
ritik-apurwa <ritikapurwa3500@gmail.com>
2023-12-14 12:45:24 +0530

    dd

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>
         <div className='bg-red-300 text-yellow-200' >
             <h1 id='pahla-h1'> First heading </h1>
-            <a href="/Home.jsx">Home</a>
+            <a href="/Home.jsx">Hoddme</a>
         </div>
     </>
   )
